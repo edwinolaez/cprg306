69ebcdafc6bcf632839586ae5560fefdb70f2bd0
Edwin Olaez <edwinolaez@yahoo.com>
2025-09-28 19:34:57 -0600

    created week4 folder and week4 link

--- assignments/app/page.js
+++ assignments/app/page.js
@@ -6,7 +6,8 @@ export default function Home() {
    <div>
     <h1>CPRG 306: Web Development 2 - Assignments</h1>
     <Link href="/week_2">Go to Week 2</Link>
-    <Link href="/week3">Got to Week3</Link>
+    <Link href="/week3">Go to Week3</Link>
+    <Link href="/week4">Go to Week 4</Link>
    </div>
   );
 }
