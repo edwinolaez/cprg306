054951cd46c27fe331c0a0642e638da69226b2d5
Edwin Olaez <edwinolaez@yahoo.com>
2025-09-30 15:14:57 -0600

    initial look of the page completed, created buttons for the add item

--- assignments/app/week4/new-item.js
+++ assignments/app/week4/new-item.js
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import Page from "../page";
+
 export default function NewItem(){
+  const [quantity, setQuantity]= useState(1);
+//  const [counter, setCounter] = useState(0)
   return (
-    <div>Add new Item</div>
+    <>
+      <div className="flex items-center gap-4">
+       <button onClick={() => setQuantity(quantity -1)} className="text-shadow-lg font-bold bg-blue-200 text-black px-7 py-7 rounded-full curser-pointer hover:bg-amber-600 hover:shadow-lg transition ease-linear duration-200" > - </button>
+      <button onClick={() => setQuantity(quantity + 1)} className="text-shadow-lg font-bold bg-green-100 text-black px-7 py-7 rounded-full curser-pointer hover:bg-amber-200 hover:shadow-lg transition ease-linear duration-200"> + </button>
+     
+      <Page quantity={quantity} />
+      </div>
+    </>
   );
 }
--- assignments/app/week4/page.js
+++ assignments/app/week4/page.js
@@ -1,12 +1,22 @@
-"useState"
+"use client"
+import { useState } from 'react';
 import NewItem from './new-item';
 
 export default function Page () {
   return (
-    <main>
+    <main className='p-8 max-w-md mx-auto'>
       <div>
-        <h1>New Item</h1>
+        <h1 className='text-shadow-blue-800 font-extrabold mb-5'> Add New Item</h1>
+        <section className='rounded border border-black p-6 bg-white'>
+          <p className='mb-6'>
+            <span className='text-2xl font-bold text-gray-600'>Quantity:</span>
+            <span className='text-2xl font-sans font-extrabold'>1</span>
+          </p>
         <NewItem/>
+        <div>
+          <p className='mt-2 text-sm text-gray-800'>Allowed Range: 1-20</p>
+        </div>
+        </section>
       </div>
     </main>
   );
