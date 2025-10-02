7743dc466af47b23d178e38d4b776488b2234006
Edwin Olaez <edwinolaez@yahoo.com>
2025-10-02 13:31:22 -0600

    final changes submission ready

--- assignments/app/week-4/new-item.js
+++ assignments/app/week-4/new-item.js
@@ -5,12 +5,12 @@ import { useState } from "react";
 export default function NewItem(){
   const [quantity, setQuantity]= useState(1);
 
-const increase = () => {
+const increment = () => {
     if(quantity < 20) {
       setQuantity(quantity +1);
     }
   };
-   const decrease = () => {
+   const decrement = () => {
     if(quantity > 1) {
       setQuantity(quantity -1);
     }
@@ -22,10 +22,10 @@ const increase = () => {
             <span className='text-2xl font-bold text-gray-600'>{quantity}:</span>
         </p>
        <div className="flex gap-4">
-        <button onClick={decrease}
+        <button onClick={decrement}
          disabled={quantity===1}
         className={`text-shadow-lg font-bold bg-blue-200 text-black px-7 py-7 rounded-xl cursor-pointer hover:bg-fuchsia-200 hover:shadow-lg transition ease-linear duration-200 ${quantity ===1 ? "opacity-50": "" }`}> - </button>
-        <button onClick={increase}
+        <button onClick={increment}
         disabled={quantity===20}
         className={`text-shadow-lg font-bold bg-green-100 text-black px-7 py-7 rounded-xl cursor-pointer hover:bg-amber-200 hover:shadow-lg transition ease-linear duration-200 ${quantity===20 ? "opacity-50" : "" }`}> + </button>
         </div>
